c7a7a7e78d39a9bbac18b789eaf224cf0979165e
kassanj <kassandram022@gmail.com>
2019-06-25 15:50:30 -0700

    create API call to Giphy and make fetch code asynchronous

--- src/App.js
+++ src/App.js
@@ -23,6 +23,29 @@ class App extends Component {
     };
   }
 
+  handleGiphy = async searchTerm => {
+    try {
+      // use fetch with our search term embedded into the `q=term` part of the url
+      const response = await fetch(        `https://api.giphy.com/v1/gifs/search?api_key=8bq9S5rnr40uJxjAfvY5FrFRGxgEkL9x&q=${
+          searchTerm
+        }&limit=50&offset=0&rating=PG-13&lang=en`)
+
+      // this grabs the results data from our json code
+      const {data} = await response.json()
+      // if we have no results in the array, we throw an error
+      // the error will be sent down to the catch part
+      if (!data.length) {
+        throw `Nothing found for ${searchTerm}`
+      }
+      // now do something with our data
+      // return doSomething(data)
+      console.log(data)
+
+    } catch (error) {
+       alert(error)
+    }
+  }
+
   handleChange = event => {
     const {value} = event.target;
 
@@ -39,8 +62,10 @@ class App extends Component {
     const {value} = event.target;
     // value gives us the previous updated value
     // which is why we need to use onChange
-    if (event.key === 'Enter') {
-      alert('you pressed enter!')
+    if (event.key === 'Enter' && value.length > 2) {
+      event.preventDefault()
+      // use our searchGiphy function above
+      this.handleGiphy(value)
     }
   }
 
