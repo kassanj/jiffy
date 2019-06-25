ad4d7d3eea658b471a1519ac80fca5f595ed8716
kassanj <kassandram022@gmail.com>
2019-06-25 13:44:03 -0700

    add event handlers

--- src/App.js
+++ src/App.js
@@ -8,9 +8,34 @@ const Header = () => (
 );
 
 function App() {
+
+  handleChange = event => {
+    const value = event.target;
+    if (value.length > 2) {
+
+    }
+  }
+
+  const handleKeyPress = event => {
+    const value = event.target;
+    // value gives us the previous updated value
+    // which is why we need to use onChange
+    if (event.key === 'Enter') {
+      alert('you pressed enter!')
+    }
+  }
+
   return (
     <div className="page">
       <Header />
+      <div className="search grid">
+        <input
+          className="input grid-item"
+          placeholder="Type something"
+          onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
+        />
+      </div>
     </div>
   );
 }
